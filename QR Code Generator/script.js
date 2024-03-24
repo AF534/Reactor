a1a49bde42dfe1e188dfe68c4393e5a02d4fa60c
Abhay Bhadoriya <bhadoriyaabhay32@gmail.com>
2024-03-24 10:58:07 +0530

    first commit

---
+++ QR Code Generator/script.js
@@ -0,0 +1,62 @@
+const wrapper = document.querySelector(".wrapper"),
+qrInput = wrapper.querySelector(".form1"),
+qrInput1 = wrapper.querySelector(".form2"),
+qrInput2 = wrapper.querySelector(".form3"),
+qrInput3 = wrapper.querySelector(".form4"),
+// qrInput2 = wrapper.querySelector(".form3"),
+// qrInput3 = wrapper.querySelector(".form4"),
+// qrInput4 = wrapper.querySelector(".form5"),
+generateBtn = wrapper.querySelector(".form button"),
+qrImg = wrapper.querySelector(".qr-code img");
+let preValue;
+let preValue1;
+let preValue2;
+let preValue3;
+
+generateBtn.addEventListener("click", () => {
+    let qrValue = qrInput.value.trim();
+    let qrValue1 = qrInput1.value.trim();
+    let qrValue2 = qrInput2.value.trim();
+    let qrValue3 = qrInput3.value.trim();
+    if(!qrValue, !qrValue1,!qrValue2,!qrValue3 || preValue === qrValue || preValue1===qrValue1 || preValue2===qrValue2 || preValue3==qrValue3) return;
+    preValue = qrValue;
+    preValue1 = qrValue1;
+    preValue2 = qrValue2;
+    preValue3 = qrValue3;
+    generateBtn.innerText = "Generating QR Code...";
+    qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${qrValue} + ${qrValue1}+ ${qrValue2} + ${qrValue3}`;
+    qrImg.addEventListener("load", () => {
+        wrapper.classList.add("active");
+        generateBtn.innerText = "Generate QR Code";
+
+        wrapper.style.height ="710px"; 
+    });
+   
+   
+  
+});
+
+qrInput.addEventListener("keyup", () => {
+    if(!qrInput.value.trim()) {
+        wrapper.classList.remove("active");
+        preValue = "";        
+    }
+});
+qrInput1.addEventListener("keyup", () => {
+    if(!qrInput1.value.trim()) {
+        wrapper.classList.remove("active");
+        preValue1 = "";        
+    }
+});
+qrInput2.addEventListener("keyup", () => {
+    if(!qrInput2.value.trim()) {
+        wrapper.classList.remove("active");
+        preValue2 = "";        
+    }
+});
+qrInput3.addEventListener("keyup", () => {
+    if(!qrInput3.value.trim()) {
+        wrapper.classList.remove("active");
+        preValue3 = "";        
+    }
+});
